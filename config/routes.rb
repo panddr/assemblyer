d61da57f82432752156c3ba2976a712d02541105
pwgen777 <andrey.cherepanov1@mail.ru>
2020-10-26 14:24:21 +0300

    add devise, admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :admins
   root to: 'homepage#ru'
   get '/en', to: 'homepage#en'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
