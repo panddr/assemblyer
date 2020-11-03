6bd08feb502805cfaaf9b8f55c428a36df2fee12
pwgen777 <andrey.cherepanov1@mail.ru>
2020-11-03 16:14:40 +0300

    localized_attribute add

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,17 +1,8 @@
 class AboutController < ApplicationController
   def index
-    @loc = I18n.locale
-    if @loc == :ru
-      @space = About.first.space_ru
-      @mission = About.first.mission_ru
-      @expert = About.first.expert_ru
-      @team = About.first.team_ru
-
-    else 
-      @space = About.first.space_en
-      @mission = About.first.mission_en
-      @expert = About.first.expert_en
-      @team = About.first.team_en
-    end
+    @space = About.first
+    @mission = About.first
+    @expert = About.first
+    @team = About.first
   end
-  end
+end
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,5 @@
 class ContactsController < ApplicationController
   def index
-    @loc = I18n.locale
-    if @loc == :ru
-      @contacts = Contact.first.caption_ru
-    else 
-      @contacts = Contact.first.caption_en
-    end
+      @contacts = Contact.first
   end
   end
