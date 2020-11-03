4c2fff509d5fcc9c849a2e7dcf4da70fdf5319b6
pwgen777 <andrey.cherepanov1@mail.ru>
2020-11-03 15:10:03 +0300

    update logic

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,11 +1,6 @@
 class HomepageController < ApplicationController
   def index
-    @loc = I18n.locale
-    if @loc == :ru
-      @pageContent = MainPage.first.caption_ru
-    else 
-      @pageContent = MainPage.first.caption_en
-    end
+    @main_page = MainPage.first
   end
   
 end
