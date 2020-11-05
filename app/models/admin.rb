2915c42293b95e25e6628720fc9bfb3be531fb17
pwgen777 <andrey.cherepanov1@mail.ru>
2020-11-05 13:00:33 +0300

    block registration new admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,4 @@
 class Admin < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :rememberable, :validatable,
+         :rememberable, remember_for: 1.year
 end
