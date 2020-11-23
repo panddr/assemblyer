3cfea4a63fd0a0fe6cd83e96479e45414ac85d53
panddr <panddr@gmail.com>
2020-11-23 15:08:47 +0300

    frontend

--- app/assets/ss_app/index.js
+++ app/assets/ss_app/index.js
@@ -5,10 +5,7 @@ import './stylesheets/index.scss';
 document.addEventListener('DOMContentLoaded', () => {
   const alert = document.querySelector('.alert');
   const notice = document.querySelector('.notice');
-  const orderSelect = document.querySelector('select#order');
-  const sortableTable = document.getElementById('sortable-body');
-  const csrfToken = $('meta[name="csrf-token"]').attr('content');
-  const sortableContributors = document.querySelector('.post_contributors');
+  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
   if (alert) {
     setTimeout(() => {
@@ -20,30 +17,5 @@ document.addEventListener('DOMContentLoaded', () => {
     setTimeout(() => {
       notice.classList.add('hide');
     }, 5000);
-  }
-
-  if (orderSelect) {
-    orderSelect.onchange = (e) => {
-      e.target.parentNode.submit();
-    };
-  }
-
-  if (sortableTable) {
-    $(sortableTable).sortable({
-      update: function(e, ui) {
-        $.ajax({
-          headers: {
-            'X-CSRF-Token': csrfToken
-          },
-          url: location.pathname + '/sort',
-          type: 'PATCH',
-          data: $(this).sortable('serialize'),
-        });
-      }
-    });
-  }
-
-  if (sortableContributors) {
-    $(sortableContributors).sortable({});
   }
 });
